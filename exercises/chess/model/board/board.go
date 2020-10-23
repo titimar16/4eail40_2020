c13ec9995c9aea134f09a1495d4820ce2c368dd9
Thibaut Maringer <17068@ecam.be>
2020-10-23 17:38:20 +0200

    Add Board test

--- exercises/chess/model/board/board.go
+++ exercises/chess/model/board/board.go
@@ -1,21 +1,21 @@
-// Package board will contain types and logic for handling chess board(s).
 package board
 
 import (
-	"fmt"
 	"github.com/chess/model/coord"
 	"github.com/chess/model/piece"
 )
 
-//Board is an interface to a chess board.
-// It defines methods for handling pieces on it.
+// Classic [8][8]piece.Piece
+type Classic [8][8]piece.Piece
 
-type Board interface {
-	fmt.Stringer
-	// PieceAt retrieves piece at give coordinates.
-	// Return nil if not piece was found.
-	PieceAt(at coord.ChessCoordinates) piece.Piece
-	// MovePiece moves a piece from given coordinate to given coordinates.
-	// Returns and error if destination was occupied
-	MovePiece(from, to coord.ChessCoordinates) error
+func (c *Classic) String() string {
+	panic("implement me")
+}
+
+func (c *Classic) PieceAt(at coord.ChessCoordinates) piece.Piece {
+	panic("implement me")
+}
+
+func (c *Classic) MovePiece(from, to coord.ChessCoordinates) error {
+	panic("implement me")
 }
