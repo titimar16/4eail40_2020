8fe6b3507c4edc233f5424e44a2705fc9227a8ab
Thibaut Maringer <17068@ecam.be>
2020-10-23 15:39:24 +0200

    Add Tests

--- exercises/chess/model/piece/piece.go
+++ exercises/chess/model/piece/piece.go
@@ -1,9 +1,27 @@
 // Package piece will handle game pieces.
 package piece
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/chess/model/coord"
+	"github.com/chess/model/player"
+)
 
 // Piece represents a game piece.
 type Piece interface {
 	fmt.Stringer
+	//
+	Color() player.Color
+	//
+	Moves() map[coord.ChessCoordinates]bool
+}
+
+func test() {
+	//var p Piece
+
+	//var move = destination - origin
+	// Check if move is contained in a map
+	//if p.Moves[move]
+
+	// HashSet
 }
---
+++ exercises/chess/model/player/color.go
@@ -0,0 +1,13 @@
+// Package player handles player color
+package player
+
+// Color represents a player color.
+// Either White, Black.
+type Color int
+
+const (
+	// White is a color.
+	White Color = iota
+	// Black is a color.
+	Black
+)
